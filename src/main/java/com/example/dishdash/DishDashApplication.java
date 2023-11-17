a20cf21612b81cc443c914a2af51a9a2cf359d9f
Alexey <alken1t15@gmail.com>
2023-11-17 23:56:06 +0600

    first commit

---
+++ src/main/java/com/example/dishdash/DishDashApplication.java
@@ -0,0 +1,13 @@
+package com.example.dishdash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DishDashApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DishDashApplication.class, args);
+    }
+
+}
